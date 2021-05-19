979c4e94f03da00c694f52d408d707171cd560ae
Jessica Jones <daddehjones@gmail.com>
2021-05-19 17:57:26 -0400

    initial commit

--- spec/animal_spec.rb
+++ spec/animal_spec.rb
@@ -1,34 +1,36 @@
 require 'rspec'
 require './lib/animal'
+require './lib/zoo'
 
 RSpec.describe Animal do
   it 'exists' do
-    animal = Animal.new("Sea Otter", 10, 25)
-
-    expect(animal).to be_a(Animal)
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    expect(animal_1).to be_a(Animal)
   end
 
-  it 'has a kind' do
-    animal = Animal.new("Sea Otter", 10, 25)
 
-    expect(animal.kind).to eq("Sea Otter")
+  it 'has a kind' do
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    expect(animal_1.kind).to eq("Sea Otter")
   end
 
   it 'has a weight' do
-    animal = Animal.new("Sea Otter", 10, 25)
-
-    expect(animal.weight).to eq("10 pounds")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    expect(animal_1.weight).to eq("10 pounds")
   end
 
   it 'has an age' do
-    animal = Animal.new("Sea Otter", 10, 25)
-
-    expect(animal.age).to eq("25 weeks")
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    expect(animal_1.age).to eq("25 weeks")
   end
 
   it 'has an age in days' do
-    animal = Animal.new("Sea Otter", 10, 25)
-
-    expect(animal.age_in_days).to eq(175)
+    animal_1 = Animal.new("Sea Otter", 10, 25)
+    animal_2 = Animal.new("Red Panda", 5, 70)
+    expect(animal_1.age_in_days).to eq(175)
   end
 end
